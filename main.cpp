5c7ef44d8f882ae4f6d02690d3de96fc6230838d
matkatmusic <>
2023-04-10 02:04:59 +0000

    Fixed Formatting issues

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-template <typename... T> void ignoreUnused(T &&...) {}
+template<typename ...T>
+void ignoreUnused(T&&...) { }
 
 /*
 Project 2 - Part 1 / 1
@@ -9,12 +10,10 @@ video: Chapter 2 - Part 3
 
  Create a branch named Part1
 
- Purpose:  This project will teach you how to declare variables and free
-functions. This will be the first project where the code you write will be
-compiled and you will be responsible for making sure it compiles before
-submitting it for review.
-
-
+ Purpose:  This project will teach you how to declare variables and free functions.  
+ This will be the first project where the code you write will be compiled and you will be responsible for making sure it compiles before submitting it for review.
+ 
+ 
  1) Write down the names of the 6 major primitive types available in C++  here:
 
 Int
@@ -24,71 +23,67 @@ Double
 Char
 Unsigned Int
 
-2) for each primitive type, write out 3 variable declarations inside the
-variableDeclaration() function on line 59. a) give each variable declaration an
-initial value
-        - just ignore wchar_t. you do not need to declare 3 variables of type
-'wchar_t'
-        - 'void' is a return type. you do not need to declare 3 variables of
-type 'void'. b) at the end of the function, call ignoreUnused once and pass all
-of your variables to it. see line 71 for an example
-
+2) for each primitive type, write out 3 variable declarations inside the variableDeclaration() function on line 59.
+    a) give each variable declaration an initial value
+        - just ignore wchar_t. you do not need to declare 3 variables of type 'wchar_t'
+        - 'void' is a return type. you do not need to declare 3 variables of type 'void'.
+    b) at the end of the function, call ignoreUnused once and pass all of your variables to it. see line 71 for an example
+ 
 3) Declare 10 free functions
-    each declaration should have a random number of parameters in the function
-parameter list. When naming your parameters, choose names that are relevant to
-the task implied by the function's name. remember: Name functions what they do
+    each declaration should have a random number of parameters in the function parameter list.
+    When naming your parameters, choose names that are relevant to the task implied by the function's name.
+    remember:
+    Name functions what they do
         takeDogForWalk(int distanceInYards);
     Name variables what they are.
         int numStepsSinceStart;
 
-4) add { ignoreUnused( ); } after each declaration in place of the closing
-semicolon 5) pass each of your function parameters to the ignoreUnused function
-like you did in b) 6) if your function returns something other than void, add
-'return { };' at the end of it. 7) provide default values for an arbitrary
-number of parameters in the function parameter list.
+4) add { ignoreUnused( ); } after each declaration in place of the closing semicolon
+5) pass each of your function parameters to the ignoreUnused function like you did in b)
+6) if your function returns something other than void, add 'return { };' at the end of it.
+7) provide default values for an arbitrary number of parameters in the function parameter list.
 
-8) consult the coding style guide found in the Readme.MD and adjust the
-formatting of your 10 functions.  At this point, you might have something that
-looks like this: float someFunc2(bool yes, int bar=2) { ignoreUnused(yes, bar );
-return { }; } This does not conform with the coding standard for this course
-(check the Readme.MD) and needs to be corrected
-
-9) in the main function at the end:
-    for each of those functions declared,
-        a) write out how the function would look if called with correct
-arguments b) if the function returned anything, store it in a local variable via
-the 'auto' keyword. c) pass the local variables to ignoreUnused() as you did in
-2b) see main() for an example of this.
-
+8) consult the coding style guide found in the Readme.MD and adjust the formatting of your 10 functions.  At this point, you might have something that looks like this:
+    float someFunc2(bool yes, int bar=2) { ignoreUnused(yes, bar ); return { }; }
+    This does not conform with the coding standard for this course (check the Readme.MD) and needs to be corrected
+    
+9) in the main function at the end: 
+    for each of those functions declared, 
+        a) write out how the function would look if called with correct arguments
+        b) if the function returned anything, store it in a local variable via the 'auto' keyword.
+        c) pass the local variables to ignoreUnused() as you did in 2b)
+    see main() for an example of this.
+ 
 10) click the [run] button.  Clear up any errors or warnings as best you can.
  */
 
 // 2)
-void variableDeclarations() {
-  // example:
-  int number = 2; // declaration of a variable named "number", that uses the primitive type 'int', and the variable's initial value is '2'
-
-  int fingers = 5;
-  int hands = 2;
-  int toes = 5;
-
-  float speed = 60.f;
-  float heading = 10.f;
-  float direction = 1.f;
-
-  bool hasPizza = false;
-  bool hasBeer = true;
-  bool hasSmile = true;
-
-  double small = 0.00f;
-  double large = 1.00f;
-  double trouble = 0.5f;
-
-  char R = 'R';
-  char Y = 'Y';
-  char A = 'A';
-
-  ignoreUnused(number, fingers, hands, toes, speed, heading, direction, hasPizza, hasBeer, hasSmile, small, large, trouble, R, Y, A);
+void variableDeclarations() 
+{
+    // example:
+    int number = 2; // declaration of a variable named "number", that uses the primitive type 'int', and the variable's initial value is '2'
+    
+    int fingers = 5;
+    int hands = 2;
+    int toes = 5;
+    
+    float speed = 60.f;
+    float heading = 10.f;
+    float direction = 1.f;
+    
+    bool hasPizza = false;
+    bool hasBeer = true;
+    bool hasSmile = true;
+    
+    double small = 0.00f;
+    double large = 1.00f;
+    double trouble = 0.5f;
+    
+    char R = 'R';
+    char Y = 'Y';
+    char A = 'A';
+    
+    ignoreUnused(number, fingers, hands, toes, speed, heading, direction, hasPizza, hasBeer, hasSmile, small, large, trouble, R, Y, A);
   // passing each variable declared to the
   // ignoreUnused() function
 }
@@ -100,8 +95,8 @@ void variableDeclarations() {
  */
 bool rentACar(int rentalDuration, int carType = 0) // function declaration with random number of arguments, arbitrary number of arguments have default value
 {
-  ignoreUnused(rentalDuration, carType); // passing each function parameter to the ignoreUnused() function
-  return {}; // if your function returns something other than void, add 'return {};' at the end of it.
+    ignoreUnused(rentalDuration, carType); // passing each function parameter to the ignoreUnused() function
+    return {}; // if your function returns something other than void, add 'return {};' at the end of it.
 }
 
 /*
@@ -200,7 +195,8 @@ char madeUpColor(char spectrum = 'foo', char band = 'bar')
  Wait for my code review.
  */
 
-int main() {
+int main() 
+{
   // example of calling that function, storing the value, and passing it to
   // ignoreUnused at the end of main()
     auto carRented = rentACar(6, 2);
